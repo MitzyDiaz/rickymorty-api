4398adcbc29f17126072b8ffde27be14ff3560eb
Mitzy Diaz <mitzy_diaz_valor@outlook.com>
2020-09-04 16:07:52 -0500

    update api component

--- src/RickmortyApi.js
+++ src/RickmortyApi.js
@@ -1,5 +1,4 @@
 import { html, css, LitElement } from 'lit-element';
-import '@polymer/paper-card/paper-card.js';
 
 export class RickmortyApi extends LitElement {
   static get styles() {
@@ -7,43 +6,43 @@ export class RickmortyApi extends LitElement {
       :host {
         display: block;
         padding: 25px;
-        color: var(--rickmorty-api-text-color, #000);
       }
     `;
   }
 
 static get properties() {
     return {
-      url: { type: String },
-      data: { type: Array },
-      limit: { type: String }
+      characters: { type: Array },
     };
   }
 
   constructor() {
     super();
-    this.url = "https://rickandmortyapi.com/api/character/";
-    this.data = [];
+    this.characters = [];
   }
 
   async firstUpdated() {
-    await fetch(`${this.url}`)
+    await fetch('https://rickandmortyapi.com/api/character/')
       .then(r => r.json())
-      .then(data => {
-        this.mapInfo(data.results);
+      .then(characters => {
+        this.mapInfo(characters.results);
       });
-    this.dispatchEvent(new CustomEvent('info-loaded', { detail: this.data }));
+    this.dispatchEvent(new CustomEvent('cargar', { detail: this.characters }));
   }
 
-  mapInfo(data) {
-    data.forEach((item, index) => {
-      let itm = {
-        name: item.name,
-        id: item.id,
-        image: item.image
+  mapInfo(characters) {
+    characters.forEach((character, index) => {
+      let rymarray = {
+        name: character.name,
+        id: character.id,
+        image: character.image
       }
-      this.data = [...this.data, itm]
+      this.characters = [...this.characters, rymarray]
     });
+  }
+
+  render(){
+    return html``;
   }
 
 }
